5a4ef2e471356aaf76548f05e88d96fc9f768fe7
Cristian Beltran <3798796+cambel@users.noreply.github.com>
2026-06-30 09:56:51 +0900

    Fix vulnerabilities

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,12 @@ import react from '@vitejs/plugin-react';
 import yaml from '@rollup/plugin-yaml';
 import path from 'path';
 
+const repoName =
+  process.env.GITHUB_REPOSITORY?.split('/')[1] ?? path.basename(__dirname);
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/CLAW/' : './',
+  base: process.env.NODE_ENV === 'production' ? `/${repoName}/` : './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
